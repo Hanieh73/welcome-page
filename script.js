93720f91a25c99d1d83260f45ddb4a9369eb2792
Hanieh73 <hanieh.zaab1@gmail.com>
2023-05-07 14:50:12 +0100

    webpage updated with the contact form and the art page

---
+++ script.js
@@ -0,0 +1,6 @@
+const myButton = document.getElementById('myButton');
+
+myButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  // Your code to handle the button click event goes here
+});
